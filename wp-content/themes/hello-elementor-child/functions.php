40529641bacbe683d7fb8d9055d6ae0042d75223
rashu-pro <rashu.pro@gmail.com>
2025-06-24 20:01:00 +0600

    sms sending api changed

--- wp-content/themes/hello-elementor-child/functions.php
+++ wp-content/themes/hello-elementor-child/functions.php
@@ -7,21 +7,3 @@ function hello_elementor_child_enqueue_styles() {
 add_action('wp_enqueue_scripts', 'hello_elementor_child_enqueue_styles');
 
 include_once ('registration-system/functions-registration-system.php');
-
-function wp_unique_username($base) {
-    $username = sanitize_user($base);
-    $suffix = 1;
-    while (username_exists($username)) {
-        $username = sanitize_user($base . $suffix++);
-    }
-    return $username;
-}
-
-function wp_unique_email($base) {
-    $email = sanitize_email($base);
-    $suffix = 1;
-    while (email_exists($email)) {
-        $email = preg_replace('/@/', $suffix++ . '@', $base, 1);
-    }
-    return $email;
-}
